7c8ee8fbd97b6fc314105939624e820b1f75ff13
tonghao,yuan <yuantonghao@gmail.com>
2020-07-21 22:23:29 +0800

    feat: add a new common header file

---
+++ common/utility.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/**
+ * @file utility.h
+ * @author tonghao.yuan (yuantonghao@gmail.com)
+ * @brief   common things not related to other catagory
+ * @version 0.1
+ * @date 2020-07-21
+ *
+ * @copyright Copyright (c) 2020
+ *
+ */
+
+template <typename T> constexpr void unused(T &&) {}
