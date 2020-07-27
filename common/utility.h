6b8e563a68e3896dec29b9cdcc697d3b4175f17e
tonghao,yuan <yuantonghao@gmail.com>
2020-07-27 22:39:14 +0800

    feat: wrapper of fmt::print with flush

--- common/utility.h
+++ common/utility.h
@@ -11,4 +11,21 @@
  *
  */
 
+#include "fmt/format.h"
+
 template <typename T> constexpr void unused(T &&) {}
+
+/**
+ * @brief Wrapper of fmt::print
+ *
+ * @tparam S
+ * @tparam Args
+ * @tparam char_t<S>
+ * @param format_str
+ * @param args
+ */
+template <typename S, typename... Args, typename Char = fmt::char_t<S>>
+inline void println(const S &format_str, Args &&... args) {
+  fmt::print(format_str, args...);
+  std::fflush(stdout);
+}
